e77e3eca61b7ce742d55f897b3cf8b26a4d6da9c
GiuntaGiovanni <jhonniest741@gmail.com>
2025-08-03 16:28:18 +0200

    send props from father component to child component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,43 @@
+
 <script setup>
-
-
+import Navbar from './Navbar.vue';
+  const navLinks = [
+    { id: 1, text: "CHARACTERS", url: "#", current: false },
+    { id: 2, text: "COMICS", url: "#", current: false },
+    { id: 3, text: "MOVIES", url: "#", current: true },
+    { id: 4, text: "TV", url: "#", current: false },
+    { id: 5, text: "GAMES", url: "#", current: false },
+    { id: 6, text: "COLLECTIBLES", url: "#", current: true },
+    { id: 7, text: "VIDEOS", url: "#", current: false },
+    { id: 8, text: "FANS", url: "#", current: true },
+    { id: 9, text: "NEWS", url: "#", current: false },
+    { id: 10, text: "SHOP", url: "#", current: true }
+  ];
 </script>
 
 <template>
-  <h1>header</h1>
+          <header class="d-flex justify-content-between align-items-center bg-light p-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col d-flex">
+                        <div class="header-content box-logo">
+                            <img src="/img/dc-logo.png" alt="DC Logo" class="dc-logo" />
+                        </div>
+                    </div>
+                    <div class="col">
+                      <Navbar :nav-links="navLinks"/>
+                    </div>
+                </div>
+            </div>
+        </header>
 </template>
 
 <style scoped>
-
+.box-logo {
+  width: 45px;
+  margin: auto 0;
+}
+.dc-logo {
+  width: 100%;
+}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<script setup>
+const props = defineProps({
+    navLinks: Array
+})
+console.log(props)
+</script>
+<template>
+        <nav class="navbar navbar-expand-md p-0">
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">prova</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">prova</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">prova</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">prova</a>
+                    </li>
+                    <!-- {props.navLinks.map(link => (
+                        <li key={link.id} class="nav-item">
+                            {/* utilizzo di un operatore ternario per attivare classe active */}
+                            <a class={`nav-link ${link.id === 3 ? 'act' : ''}`} href={link.url}>{link.text}</a>
+                        </li>
+                    ))} -->
+                </ul>
+            </div>
+        </nav>
+</template>
+
+<style scoped>
+/* Navbar */
+.navbar-nav {
+  margin: 0;
+  padding: 0;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.navbar-nav .nav-link {
+  font-size: 1rem;
+  font-family: "Open Sans";
+  color: #000;
+  padding: 1.2rem;
+}
+
+/* Navbar Effetto Hover */
+.navbar-nav .nav-link:hover {
+  color: #007bff;
+}
+
+/* Effetto Active */
+.navbar-nav .act {
+  color: #007bff;
+  border-bottom: 1px solid #007bff;
+}
+</style>
