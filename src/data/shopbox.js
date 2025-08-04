5c3b21d167eacbe4953d1d204434d8efba296c05
GiuntaGiovanni <jhonniest741@gmail.com>
2025-08-04 13:00:06 +0200

    Feat: create App Cards and add Data comics & shopBox

---
+++ src/data/shopbox.js
@@ -0,0 +1,29 @@
+const shopBox = [
+    {
+        id: 1,
+        imgSrc: './img/buy-comics-digital-comics.png',
+        label: 'DIGITAL COMICS'
+    },
+    {
+        id: 2,
+        imgSrc: './img/buy-comics-merchandise.png',
+        label: 'DC MERCHANDISE'
+    },
+    {
+        id: 3,
+        imgSrc: './img/buy-comics-subscriptions.png',
+        label: 'SUBSCRIPTION'
+    },
+    {
+        id: 4,
+        imgSrc: './img/buy-comics-shop-locator.png',
+        label: 'COMIC SHOP LOCATOR'
+    },
+    {
+        id: 5,
+        imgSrc: './img/buy-dc-power-visa.svg',
+        label: 'DC POWER VISA'
+    }
+];
+
+export default shopBox
